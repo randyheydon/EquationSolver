f714f26a576c852856805cd359496a8c22d3c945
Randy Heydon <randy.heydon@clockworklab.net>
2017-09-17 08:50:23 -0400

    Redid method documentation using XML syntax.
    Recently discovered that this is a typical way to produce structured
    documentation for C# methods.  Allows documentation to be automatically
    processed by IDEs and other tools, so users of these methods don't need to dig
    through the code for the unstructured comments I made before.

--- NewtonSolver/NewtonSolver.cs
+++ NewtonSolver/NewtonSolver.cs
@@ -13,18 +13,52 @@ namespace NewtonSolver
 		// such functions.
 		public delegate double MathFunc(double x);
 
-		// Create an approximate derivative function of a given function, using a difference
-		// of size epsilon.
-		// Calculating analytic derivatives is a pain without a proper computer algebra
-		// system.  A numerical approximation with the finite difference method is much
-		// easier.
+		/// <summary>
+		/// Create an approximate derivative function (a.k.a. Jacobian) of a given function.
+		/// </summary>
+		/// <remarks>
+		/// Calculating analytic derivatives is a pain without a proper computer algebra
+		/// system.  A numerical approximation with the finite difference method is much
+		/// easier.
+		/// </remarks>
+		/// <returns>A MathFunc representing the approximated derivative function.</returns>
+		/// <param name="function">The MathFunc to differentiate.</param>
+		/// <param name="epsilon">
+		/// The size of the finite difference.  Smaller values will give results closer to
+		/// the analytic derivative, but with more risk of numerical error.
+		/// </param>
 		public static MathFunc FiniteDifference(MathFunc function, double epsilon = 0.0001)
 		{
 			return (x) => (function(x + epsilon / 2.0) - function(x-epsilon/2.0)) / epsilon;
 		}
 
-		// Use Newton's method to find a root of a given function with a given derivative
-		// function (also known as a Jacobian) and a given starting guess.
+		/// <summary>
+		/// Finds a root of the given function (i.e. the input needed to set the function to
+		/// zero) using Newton's method.
+		/// </summary>
+		/// <returns>A value that will set the given function to approximately zero.</returns>
+		/// <param name="function">The MathFunc to be solved.</param>
+		/// <param name="jacobian">
+		/// The derivative of the given function.  Also known as the Jacobian of the
+		/// function.
+		/// </param>
+		/// <param name="guess">
+		/// Starting point for the solving process.  The closer it is to a solution, the
+		/// quicker the solution process will be.
+		/// </param>
+		/// <param name="maxerr">
+		/// Maximum acceptable error in the solution.  The final result of the input
+		/// function must be zero plus or minus this error value.
+		/// </param>
+		/// <param name="maxiters">
+		/// Maximum number of iterations to perform before giving up.  Newton's method is
+		/// an iterative process, but it can't always find a solution, so it must be
+		/// allowed to fail eventually.
+		/// </param>
+		/// <exception cref="SolveFailureException">
+		/// Thrown if a solution cannot be found to the desired error level within the
+		/// desired number of iterations.
+		/// </exception>
 		public static double NewtonSolve(MathFunc function, MathFunc jacobian, double guess,
 		                                 double maxerr = 1e-6, int maxiters = 1000)
 		{
@@ -46,7 +80,19 @@ namespace NewtonSolver
 			throw new SolveFailureException("Could not find solution");
 		}
 
-		// If no Jacobian is given, use FiniteDifference to generate an approximation.
+		/// <summary>
+		/// Find a root of the given function using Newton's method, with a derivative
+		/// approximated using the FiniteDifference method.
+		/// </summary>
+		/// <returns>A value that will set the given function to approximately zero.</returns>
+		/// <param name="function">The MathFunc to be solved.</param>
+		/// <param name="guess">
+		/// Starting point for the solving process.  The closer it is to a solution, the
+		/// quicker the solution process will be.
+		/// </param>
+		/// <exception cref="SolveFailureException">
+		/// Thrown if a solution cannot be found.
+		/// </exception>
 		public static double NewtonSolve(MathFunc function, double guess)
 		{
 			return NewtonSolve(function, FiniteDifference(function), guess);
