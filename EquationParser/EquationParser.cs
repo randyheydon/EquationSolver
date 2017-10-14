b1e845f39948afeef071c1783de927b5034e916f
Randy Heydon <randy.heydon@clockworklab.net>
2017-10-14 14:55:46 -0400

    Rough implementation of unsafe code-based equation parser.
    Hacked this together based on a StackOverflow example, and it works even though
    I don't fully understand it.  Will need to add error handling for the generated
    code, plus tests and thorough documentation, but committing now because who
    knows when I will get to that.

--- EquationParser/EquationParser.cs
+++ EquationParser/EquationParser.cs
@@ -1,10 +1,34 @@
 ﻿using System;
 namespace EquationParser
 {
-	public class MyClass
+	public class UnsafeEquationParser
 	{
-		public MyClass()
+		private const string code_template = @"
+			public class EquationWrapper
+			{{
+				public static double Equation(double x)
+				{{
+					return {0};
+				}}
+			}}";
+
+		private System.Reflection.MethodInfo func;
+
+		public UnsafeEquationParser(string equation)
 		{
+			// Dynamically builds code based on the equation, using a technique adapted from
+			// https://stackoverflow.com/questions/3188882/compile-and-run-dynamic-code-without-generating-exe
+			var code = string.Format(code_template, equation);
+			var compiled = new Microsoft.CSharp.CSharpCodeProvider().CompileAssemblyFromSource(
+				new System.CodeDom.Compiler.CompilerParameters { GenerateInMemory = true },
+				code);
+			func = compiled.CompiledAssembly.GetType("EquationWrapper").GetMethod("Equation");
+		}
+
+		public double Evaluate(double x)
+		{
+			var result = func.Invoke(null, new object[] { x });
+			return Convert.ToDouble(result);
 		}
 	}
 }
